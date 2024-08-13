134f61335a76100b52a228ca21edc5af8ab83f4c
unknown <kowsalyalalitha17@gmail.com>
2024-08-13 18:26:18 +0530

    first commit

--- src/Components/Navbars.jsx
+++ src/Components/Navbars.jsx
@@ -1,49 +1,3 @@
-// import React, { useState } from 'react';
-// import MyResumeImgs from '../assets/Images/My-Pic2.jpeg';
-
-// function Navbars() {
-//   const [menuOpen, setMenuOpen] = useState(false);
-
-//   const toggleMenu = () => {
-//     setMenuOpen(!menuOpen);
-//   };
-
-//   const handleScrollToSection = (sectionId) => (event) => {
-//     event.preventDefault();
-//     const section = document.getElementById(sectionId);
-//     if (section) {
-//       section.scrollIntoView({ behavior: 'smooth' });
-//     }
-//     setMenuOpen(false);
-//   };
-
-//   return (
-//     <div className='Navbars-Css'>
-//       <nav className='navbar'>
-//         <div className="hamburger" onClick={toggleMenu}>
-//           <span className="bar"></span>
-//           <span className="bar"></span>
-//           <span className="bar"></span>
-//         </div>
-//         <ul className={`nav-menu ${menuOpen ? 'open' : ''}`}>
-//           <li><a href="#" onClick={handleScrollToSection('top')}>Home</a></li>
-//           <li><a href="#" onClick={handleScrollToSection('about-section')}>About</a></li>
-//           <li><a href="#" onClick={handleScrollToSection('skills-section')}>Skills</a></li>
-//           <li><a href="#" onClick={handleScrollToSection('projects-section')}>Projects</a></li>
-//           <li><a href="#" onClick={handleScrollToSection('contact-section')}>Contact</a></li>
-//           <li><img src={MyResumeImgs} className='resume-Css' alt="resume-pic" onClick={handleScrollToSection('about-section')} /></li>
-//         </ul>
-//       </nav>
-//       <div className='Heading-Type'>
-//         <h2 className='Myworld-style'>WELCOME TO MY WORLD</h2>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Navbars;
-
-
 import React, { useState } from 'react';
 import MyResumeImgs from '../assets/Images/My-Pic2.jpeg';
 
@@ -64,7 +18,7 @@ function Navbars() {
         section.scrollIntoView({ behavior: 'smooth' });
       }
     }
-    setMenuOpen(false);
+    setMenuOpen(false); // Close menu after clicking a link
   };
 
   return (
--- src/Components/Typer.jsx
+++ src/Components/Typer.jsx
@@ -1,15 +1,80 @@
+// import React from 'react';
+
+// const TYPING_SPEED = 150;
+// const DELETING_SPEED = 30;
+
+// class Typer extends React.Component {
+
+//     state = {
+//     text: '',
+//     isDeleting: false,
+//     loopNum: 0,
+//     typingSpeed: TYPING_SPEED
+//   };
+
+//   componentDidMount() {
+//     this.handleType();
+//   }
+
+//   handleType = () => {
+//     const { dataText } = this.props;
+//     const { isDeleting, loopNum, text, typingSpeed } = this.state;
+//     const i = loopNum % dataText.length;
+//     const fullText = dataText[i];
+
+//     this.setState({
+//       text: isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1),
+//       typingSpeed: isDeleting ? DELETING_SPEED : TYPING_SPEED
+//     });
+
+//     if (!isDeleting && text === fullText) {
+//       setTimeout(() => this.setState({ isDeleting: true }), 500);
+//     } else if (isDeleting && text === '') {
+//       this.setState({
+//         isDeleting: false,
+//         loopNum: loopNum + 1
+//       });
+//     }
+
+//     setTimeout(this.handleType, typingSpeed);
+//   };
+
+//   handleHireMeClick = () => {
+//     // Scroll to the Contact Me section
+//     const contactMeSection = document.getElementById('contact-me-section');
+//     if (contactMeSection) {
+//       contactMeSection.scrollIntoView({ behavior: 'smooth' });
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>
+//           {this.props.heading}&nbsp;
+//           <span>{this.state.text}</span>
+//         </h1>
+//         <button type="button" className='Hire-me-Css' onClick={this.handleHireMeClick}>Hire Me</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Typer;
+
+
+/* Modify your React component */
 import React from 'react';
 
 const TYPING_SPEED = 150;
 const DELETING_SPEED = 30;
 
 class Typer extends React.Component {
-
   state = {
     text: '',
     isDeleting: false,
     loopNum: 0,
-    typingSpeed: TYPING_SPEED
+    typingSpeed: TYPING_SPEED,
   };
 
   componentDidMount() {
@@ -23,8 +88,10 @@ class Typer extends React.Component {
     const fullText = dataText[i];
 
     this.setState({
-      text: isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1),
-      typingSpeed: isDeleting ? DELETING_SPEED : TYPING_SPEED
+      text: isDeleting
+        ? fullText.substring(0, text.length - 1)
+        : fullText.substring(0, text.length + 1),
+      typingSpeed: isDeleting ? DELETING_SPEED : TYPING_SPEED,
     });
 
     if (!isDeleting && text === fullText) {
@@ -32,7 +99,7 @@ class Typer extends React.Component {
     } else if (isDeleting && text === '') {
       this.setState({
         isDeleting: false,
-        loopNum: loopNum + 1
+        loopNum: loopNum + 1,
       });
     }
 
@@ -40,7 +107,6 @@ class Typer extends React.Component {
   };
 
   handleHireMeClick = () => {
-    // Scroll to the Contact Me section
     const contactMeSection = document.getElementById('contact-me-section');
     if (contactMeSection) {
       contactMeSection.scrollIntoView({ behavior: 'smooth' });
@@ -52,9 +118,15 @@ class Typer extends React.Component {
       <div>
         <h1>
           {this.props.heading}&nbsp;
-          <span>{this.state.text}</span>
+          <span className="typing-span">{this.state.text}</span>
         </h1>
-        <button type="button" className='Hire-me-Css' onClick={this.handleHireMeClick}>Hire Me</button>
+        <button
+          type="button"
+          className="Hire-me-Css"
+          onClick={this.handleHireMeClick}
+        >
+          Hire Me
+        </button>
       </div>
     );
   }
